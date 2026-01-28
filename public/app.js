12a1778a2087e93da435c6bdde1f4e7640672630
bellbits <bellbits@gmail.com>
2026-01-28 12:48:27 -0500

    reverted it all back

--- public/app.js
+++ public/app.js
@@ -36,7 +36,6 @@ function renderEvents() {
     card.innerHTML = `
       <button class="delete-btn">✕</button>
       <h3>${event.description}</h3>
-      ${event.time}
       <div class="time" id="time-${index}"></div>
     `;
 
